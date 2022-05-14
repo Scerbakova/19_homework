da9ad0c64128c000c0ffd30a2bd635776dc58595
Scerbakova <reginadilevka@inbox.lv>
2022-05-14 14:01:39 +0300

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  decrement, devide, increment, incrementByAmount, multiply, reset,
+} from './store/reducers/counterSlice';
+import { RootState, AppDispatch } from './store';
+// import styles from './Counter.module.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch<AppDispatch>();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <h1>{count}</h1>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(incrementByAmount(5))}
+        >
+          Increment by 5
+        </button>
+
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(devide())}
+        >
+          devide by 2
+        </button>
+
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(multiply())}
+        >
+          Multiply by 7
+        </button>
+
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(reset(0))}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
